d1148632d1078898884d7a3bcf5b87d069d81f4c
Kelvin Hsu <kelvin.hsu@binarystar.ca>
2024-10-05 12:07:58 -0300

    unlink external package

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,33 +1,33 @@
-import { secretService } from '$lib/yizySpec/examples/secretService';
-//import { classifiiApi } from '$lib/yizySpec/examples/classifiiSpec';
-//import { type Service } from '$lib/yizySpec/YIZYSpec';
-import { derived, readonly, writable } from 'svelte/store';
-import YAML from 'yaml';
-import YIZY from 'yizy';
+import { secretService } from "$lib/yizySpec/examples/secretService";
+//import { classifiiApi } from "$lib/yizySpec/examples/classifiiSpec";
+import { type Service } from "$lib/yizySpec/YIZYSpec";
+import { derived, readonly, writable } from "svelte/store";
+import YAML from "yaml";
+//import YIZY from 'yizy';
 
 //const _service = writable<Service>(classifiiApi);
-const _service = writable<YIZY.Service>(secretService);
+const _service = writable<Service>(secretService);
 export const currentService = readonly(_service);
 
 export const serviceInJson = derived(currentService, ($currentService) => {
-	return JSON.stringify($currentService, null, 4);
+  return JSON.stringify($currentService, null, 4);
 });
 
 export const serviceInYaml = derived(currentService, ($currentService) => {
-	return YAML.stringify($currentService, {
-		indent: 4
-	});
+  return YAML.stringify($currentService, {
+    indent: 4,
+  });
 });
 
 //UI Level Functions
 export function importService(jsonString: string) {
-	const service: YIZY.Service = JSON.parse(jsonString);
-	updateService(service);
+  const service: Service = JSON.parse(jsonString);
+  updateService(service);
 }
 
 //State Level Functions
-function updateService(newService: YIZY.Service) {
-	_service.update(() => {
-		return newService;
-	});
+function updateService(newService: Service) {
+  _service.update(() => {
+    return newService;
+  });
 }
