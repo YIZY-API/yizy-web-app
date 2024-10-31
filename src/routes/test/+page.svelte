aff803ff9a1df3e45061c12e35b9c7eed4e1a1d8
Kelvin Hsu <kelvin.hsu@binarystar.ca>
2024-10-31 16:40:59 -0300

    implemented drag and drop

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { GripVertical, PlusIcon } from 'lucide-svelte';
+	import { twMerge } from 'tailwind-merge';
+	//import { v4 as uuidv4 } from 'uuid';
 	interface Item {
 		id: number;
 		text: string;
@@ -11,16 +13,19 @@
 	];
 
 	let draggedItem: Item | null = null;
+	let currentDropzoneIndex: number | null = null;
 
 	function handleDragStart(e: DragEvent, item: Item) {
+		e.dataTransfer!.setDragImage(document.getElementById(item.id.toString())!, 30, 5);
 		draggedItem = item;
 		e.dataTransfer!.effectAllowed = 'move';
-		e.target.classList.add('dragging');
+		(e.target! as HTMLElement).classList.add('dragging');
 	}
 
 	function handleDragEnd(e: DragEvent) {
-		e.target.classList.remove('dragging');
+		(e.target! as HTMLElement).classList.remove('dragging');
 		draggedItem = null;
+		currentDropzoneIndex = null;
 	}
 
 	function handleDragOver(e: DragEvent, index: number) {
@@ -28,10 +33,14 @@
 		const draggingElement = document.querySelector('.dragging');
 
 		if (draggingElement) {
-			const currentItems = items.filter((item) => item !== draggedItem);
-			currentItems.splice(index, 0, draggedItem!);
-			items = currentItems;
+			currentDropzoneIndex = index;
 		}
+	}
+
+	function handleDrop(e: DragEvent, index: number) {
+		const currentItems = items.filter((item) => item !== draggedItem);
+		currentItems.splice(index, 0, draggedItem!);
+		items = currentItems;
 	}
 
 	function addNewItem(fromIndex: number) {
@@ -49,27 +58,30 @@
 <div class="w-full pb-8 pt-24">
 	<div>
 		{#each items as item, index}
-			<div class=" group flex flex-row">
+			<div
+				id={item.id.toString()}
+				role="none"
+				class={twMerge(
+					'group flex flex-row',
+					index === currentDropzoneIndex && 'border-b-2 border-primary'
+				)}
+				ondragstart={(e) => handleDragStart(e, item)}
+				ondragend={(e) => handleDragEnd(e)}
+				ondragover={(e) => handleDragOver(e, index)}
+				ondrop={(e) => handleDrop(e, index)}
+			>
 				<button class="flex h-6 w-6" onclick={() => addNewItem(index + 1)}>
 					<PlusIcon
 						class="m-auto h-4 text-transparent hover:text-muted focus:text-muted active:text-muted group-hover:text-muted"
 					/>
 				</button>
 
-				<div
-					role="list"
-					class="flex h-6 w-6 hover:cursor-grab"
-					draggable={true}
-					ondragstart={(e) => handleDragStart(e, item)}
-					ondragend={handleDragEnd}
-					ondragover={(e) => handleDragOver(e, index)}
-				>
+				<div role="list" class="flex h-6 w-6 hover:cursor-grab" draggable={true}>
 					<GripVertical
 						class="m-auto h-4 text-transparent hover:text-muted focus:text-muted active:text-muted group-hover:text-muted"
 					/>
 				</div>
 
-				<!-- List item -->
 				<div>
 					{item.text}
 				</div>
